6bd7121b52f6b65e09339f032547ff7c3ca81ae8
Ashish Gaurav <agaurav77@yahoo.com>
2016-04-08 01:48:32 +0530

    basic page showing working

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
+var swig = require('swig');
 
-mongoose.connect('mongodb://localhost/nodewiki');
 app.use(morgan('dev'));
+mongoose.connect('mongodb://localhost/nodewiki');
 
-var Page = mongoose.model('Page', {
+var pageSchema = mongoose.Schema({
   url : String,
   title : String,
   created : Date,
@@ -14,22 +15,31 @@ var Page = mongoose.model('Page', {
   text : String
 });
 
-app.get('/api/', function(req, res) {
+var Page = mongoose.model('Page', pageSchema);
+
+app.get('/', function(req, res) {
   Page.find(function(err, pages) {
     if (err) res.send(err);
     res.json(pages);
   });
 });
 
-app.get('/api/:url', function(req, res) {
-  Page.findOne({url : req.params.url}, function(err, page) {
+app.get('/:url', function(req, res) {
+  console.log(req.params.url);
+  Page.findOne({url: req.params.url}, function(err, page) {
+    console.log(page);
     if (err) res.send(err);
-    res.json(page);
+    if (page)
+      res.end(swig.renderFile('./index.html', {
+        title: page.title,
+        text: page.text
+      }));
+    else
+      res.end(swig.renderFile('./index.html', {
+        title: req.params.url+' not found',
+        text: 'This page was not found.'
+      }));
   });
-});
-
-app.get('*', function(req, res) {
-  res.sendFile(__dirname+'/public/index.html');
 });
 
 app.listen(8080);
