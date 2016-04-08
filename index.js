03447035317a37fe37b52dffb62b69f95b7d7c41
Ashish Gaurav <agaurav77@yahoo.com>
2016-04-08 06:50:51 +0530

    simple add pages, edit pages working

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ var app = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var swig = require('swig');
+var bodyParser = require('body-parser');
+
+// extended global replace
+String.prototype.replaceAll = function(search, replacement) {
+  var target = this;
+  return target.replace(new RegExp(search, 'g'), replacement);
+};
 
 app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost/nodewiki');
 
 var pageSchema = mongoose.Schema({
@@ -24,22 +34,47 @@ app.get('/', function(req, res) {
 });
 
 app.get('/:url', function(req, res) {
-  console.log(req.params.url);
   Page.findOne({url: req.params.url}, function(err, page) {
-    console.log(page);
     if (err) res.send(err);
-    if (page)
+    if (page) {
       res.end(swig.renderFile('./index.html', {
+        linktext: "Edit this page",
+        link: "/edit/"+page.url,
         title: page.title,
-        text: page.text
+        text: page.text.replaceAll('\r\n', "<br>")
       }));
-    else
-      res.end(swig.renderFile('./index.html', {
-        title: req.params.url+' not found',
-        text: 'This page was not found.'
-      }));
+    } else {
+      res.redirect('/edit/'+req.params.url);
+    }
   });
 });
 
+app.get('/edit/:url', function(req, res) {
+  Page.findOne({url: req.params.url}, function(err, page) {
+    if (err) res.send(err);
+    res.end(swig.renderFile('./edit.html', {
+      url: req.params.url,
+      title: (page ? page.title : ""),
+      text: (page ? page.text : ""),
+      keepdiscard: (page? true : false)
+    }));
+  });
+});
+
+app.post('/save/:url', function(req, res) {
+  var wanted = Page.findOne({url: req.params.url}, function(err, page) {
+    if (err) res.send(err);
+    if (!page) {
+      page = new Page({url: req.params.url});
+    }      
+    page.title = req.body.title;
+    page.text = req.body.text;
+    page.save(function(err, page) {
+      if (err) res.send(err);
+      res.redirect('/'+req.params.url);
+    });
+  });
+});
+    
 app.listen(8080);
 console.log('Listening...');
